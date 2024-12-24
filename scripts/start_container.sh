e5ff375a99f2c9e065c88db553ab555c5cc2abe0
sakthitechies <69077070+sakthitechies@users.noreply.github.com>
2024-12-24 13:42:44 +0530

    Updated correct repo start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sakthi0927/simple-python-flask-app
+docker pull sakthi0927/code-build-repo/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sakthi0927/simple-python-flask-app
+docker run -d -p 5000:5000 sakthi0927/code-build-repo/simple-python-flask-app
