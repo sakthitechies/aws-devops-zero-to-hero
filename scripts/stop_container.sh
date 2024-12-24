7dce4dda8b371b4f22c349ef8465191ec1b971d0
sakthitechies <69077070+sakthitechies@users.noreply.github.com>
2024-12-24 19:23:03 +0530

    Updated to check any container is running or not stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,5 +3,9 @@ set -e
 
 # Stop the running container (if any)
 containerid=$(sudo docker ps | awk 'NR>1 {print $1}')
+if[ -z "$containerid" ]; then
+   echo "No containers running to stop"
+   exit 0
+fi
 
-sudo docker stop $containerid
+sudo docker stop "$containerid"
